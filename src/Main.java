9dac1545a6c2f089a546a95fde4528c4b2d743cd
Kostromina Alina <a.kstrmna95@gmail.com>
2023-09-10 20:33:42 +0700

    Task resolved

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,25 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        // Объявляете переменные для входных данных и
+        // параметров программы: начального счёта,
+        // суммы пополнения и тп
 
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
+        // Условным оператором проверяете, превысила ли
+        // сумма пополнения порог, и для этих двух разных
+        // сценариев рассчитываете сумму бонуса и выводите
+        // на экран.
+
+        int currentAmount = 200;
+
+        int addedAmount = 6000;
+
+        int conditionToAdd = 100;
+
+        if (addedAmount > 1000) {
+            System.out.println(currentAmount + addedAmount + (addedAmount / conditionToAdd));
+        } else {
+            System.out.println(currentAmount + addedAmount);
         }
     }
 }
